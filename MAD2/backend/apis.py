baf3258dd6041429850a2bc95f087e7ab6e6b85e
S SHRIPRASAD <22f1000598@ds.study.iitm.ac.in>
2023-11-11 23:13:00 +0530

    API addition sample

--- MAD2/backend/apis.py
+++ MAD2/backend/apis.py
@@ -1,5 +1,36 @@
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from models import *
+from flask import request
+
 class Homepage(Resource):
     def get(self):
         return {"hello": "World"}
 
+user_data= reqparse.RequestParser()
+
+user_data.add_argument('fname')
+user_data.add_argument('lname')
+user_data.add_argument('mobile')
+user_data.add_argument('email')
+user_data.add_argument('password')
+
+user_fields= {
+    'fname': fields.String,
+    'lname': fields.String,
+    'mobile': fields.Integer,
+    'email': fields.String
+}
+
+class USERApi(Resource):
+    def get(self):
+        return {"hello": "User"}
+
+    @marshal_with(user_fields)
+    def post(self):
+        args=user_data.parse_args()
+        print(args)
+
+        user= User(fname=args['fname'], lname= args['lname'], mobile=args['mobile'], email=args['email'])
+        session.add(user)
+        session.commit()
+        return user
--- MAD2/backend/app.py
+++ MAD2/backend/app.py
@@ -3,10 +3,10 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__, template_folder="templates")
 from models import *
-from apis import Homepage
+from apis import *
 
 api = Api(app)
 api.add_resource(Homepage,'/')
-
+api.add_resource(USERApi, '/user')
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
